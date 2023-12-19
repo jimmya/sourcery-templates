9cc0be425a9d317f7ce3b2ac1644443eeaf3825c
Wouter Vermeij <woutervermeij@hotmail.com>
2023-12-19 22:05:08 +0100

    remove file template comments

--- Sources/Templates/Extensions/Sequence+Extension.swift
+++ Sources/Templates/Extensions/Sequence+Extension.swift
@@ -1,12 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Wouter Vermeij on 19/12/2023.
-//
-
-import Foundation
-
 extension Sequence where Iterator.Element: Hashable {
     func unique() -> [Iterator.Element] {
         var seen: Set<Iterator.Element> = []
